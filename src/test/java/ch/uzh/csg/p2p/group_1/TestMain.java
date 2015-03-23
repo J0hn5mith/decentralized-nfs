f0c1de4ae9d833e1657a01267ef5a2c13467836f
Jan Meier <janmeier0.o@gmail.com>
2015-03-23 19:28:19 +0100

    added successing and failing test

--- src/test/java/ch/uzh/csg/p2p/group_1/TestMain.java
+++ src/test/java/ch/uzh/csg/p2p/group_1/TestMain.java
@@ -4,14 +4,22 @@
 package ch.uzh.csg.p2p.group_1;
 
 
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMain {
+
     @Test
-    public void testPrintHelloWorld() {
+    public void failingTest() {
 
         Assert.assertEquals(0, 1);
+
+    }
+
+    @Test
+    public void succeedingTest() {
+
+        Assert.assertEquals(1, 1);
 
     }
 
