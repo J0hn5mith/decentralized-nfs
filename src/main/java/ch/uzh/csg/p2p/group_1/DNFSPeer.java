474805b54432f802cfe2e6ab0fa4d401573916b7
henry <henry@henry-HP-EliteBook-840-G2>
2015-05-07 15:43:40 +0200

    Created DNFSNetwork and DNFSPeer

--- src/main/java/ch/uzh/csg/p2p/group_1/DNFSPeer.java
+++ src/main/java/ch/uzh/csg/p2p/group_1/DNFSPeer.java
@@ -1,117 +1,86 @@
 package ch.uzh.csg.p2p.group_1;
 
-import net.tomp2p.dht.FutureGet;
-import net.tomp2p.dht.FuturePut;
-import net.tomp2p.dht.PeerBuilderDHT;
-import net.tomp2p.dht.PeerDHT;
-import net.tomp2p.p2p.PeerBuilder;
-import net.tomp2p.peers.Number160;
-import net.tomp2p.storage.Data;
-
 import java.io.IOException;
-import java.util.*;
 
-/**
- * Created by janmeier on 02.04.15.
- * WARN: Out of date, no longer used
- */
-public class DNFSPeer implements DNFSIPeer{
+import net.tomp2p.peers.Number160;
 
-    private PeerDHT peer;
-
-
-    /**
-     * 
-     */
-
-    /**
-     * 
-     * @throws IOException
-     */
-    public void setUp() throws IOException {
-        final Random RND = new Random(42L);
-        this.peer = new PeerBuilderDHT(new PeerBuilder(new Number160(RND)).ports(6111988).start()).start();
+public class DNFSPeer implements DNFSIPeer {
+    
+    
+    public void setup(int port) {
+        try {
+            DNFSNetwork.setup(port);
+            DNFSNetwork.start();
+        } catch(Exception e) {
+            System.err.println(e.toString());
+        }
     }
-
-
-    /**
-     * 
-     * @param path
-     * @param file
-     * @return
-     * @throws IOException
-     */
-    public FuturePut putFile(String path, String file) throws IOException {
-        DNFSData<String> data = new DNFSData<String>(path, file);
-        return peer.put(this.createKey(path)).data(new Data(data)).start();
-    }
-
-    /**
-     * 
-     * @param path
-     * @return
-     */
-    public FutureGet getFile(String path) {
-        return this.peer.get(this.createKey(path)).start();
-    }
-
-    /**
-     * 
-     * @param key
-     * @return
-     */
-    private Number160 createKey(String key) {
-        return Number160.createHash(key);
-    }
+    
 
     @Override
     public DNFSBlock createBlock() {
+        // TODO Auto-generated method stub
         return null;
     }
 
+    @Override
     public DNFSBlock getBlock(Number160 id) {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public void updateBlock(DNFSBlock block) {
+        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void deleteBlock(Number160 id) {
+        // TODO Auto-generated method stub
 
     }
 
-
     @Override
     public DNFSiNode createINode() {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public DNFSiNode getINode(Number160 iNodeID) throws DNFSException {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public void deleteINode(Number160 iNodeID) {
+        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void updateINode(DNFSiNode iNode) {
+        // TODO Auto-generated method stub
 
     }
 
-
     @Override
     public DNFSiNode getRootINode() throws DNFSException {
+        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public DNFSiNode createRootINode() throws DNFSException {
+        // TODO Auto-generated method stub
         return null;
     }
+
+    @Override
+    public void setUp() throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
 }
